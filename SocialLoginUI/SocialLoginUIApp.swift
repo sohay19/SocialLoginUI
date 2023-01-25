be29c840dca50d46d49fdd10d888f33d034a341c
soha <sa4022@naver.com>
2023-01-25 18:01:57 +0900

    Initial Commit

---
+++ SocialLoginUI/SocialLoginUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialLoginUIApp.swift
+//  SocialLoginUI
+//
+//  Created by 소하 on 2023/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SocialLoginUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
